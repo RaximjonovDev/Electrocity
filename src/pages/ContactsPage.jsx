5a92ae1d6f8ab468695edde53efb68ae23004b87
RaximjonovDev <abdulazizraximjonov7@gmail.com>
2024-04-27 20:11:31 +0500

    Contacts Page

---
+++ src/pages/ContactsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Contacts from '../components/Contacts'
+import InformationComp from '../components/InformationComp'
+
+const ContactsPage = () => {
+  return (
+    <>
+    <Contacts/>
+    <InformationComp/>
+    </>
+  )
+}
+
+export default ContactsPage
